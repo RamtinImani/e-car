00d4f3e7b97f1fef52dc9177f86ffa64bd1d1de3
Ramtin Imani <ramtinimani.dev@gmail.com>
2024-10-11 19:57:50 +0330

    added show and hide menu js

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+/*=============== SHOW MENU ===============*/
+const toggleBtn = document.getElementById("nav-toggle");
+const navMenu = document.getElementById("nav-menu");
+const closeBtn = document.getElementById("nav-close");
+// show menu
+if (toggleBtn) {
+  toggleBtn.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+// hidden menu
+if (closeBtn) {
+  closeBtn.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
