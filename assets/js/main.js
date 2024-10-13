267003c32b04d93484896816aa48b6586aa8991b
Ramtin Imani <ramtinimani.dev@gmail.com>
2024-10-13 09:54:18 +0330

    added show scroll up button js

--- assets/js/main.js
+++ assets/js/main.js
@@ -68,3 +68,12 @@ function activeFeatured() {
   this.classList.add("active-featured");
 }
 linkFeatured.forEach((link) => link.addEventListener("click", activeFeatured));
+/*=============== SHOW SCROLL UP ===============*/
+const scrollUp = () => {
+  const scrollUp = document.getElementById("scroll-up");
+  // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+  this.scrollY >= 350
+    ? scrollUp.classList.add("show-scroll")
+    : scrollUp.classList.remove("show-scroll");
+};
+window.addEventListener("scroll", scrollUp);
